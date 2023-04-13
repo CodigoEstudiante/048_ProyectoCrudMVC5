e87ff99d7b4f8267cfecab3bb52a0d40474178c1
jose chambi <jose.chambi@ingrammicro.com>
2023-04-12 23:10:25 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoCrud/Controllers/ContactoController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ProyectoCrud.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace ProyectoCrud.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private static string conexion = ConfigurationManager.ConnectionStrings["cadena"].ToString();
+
+        private static List<Contacto> olista = new List<Contacto>();
+
+        // GET: Contacto
+        public ActionResult Inicio()
+        {
+
+            olista = new List<Contacto>();
+
+            using (SqlConnection oconexion = new SqlConnection(conexion) ) {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM CONTACTO", oconexion);
+                cmd.CommandType = CommandType.Text;
+                oconexion.Open();
+
+                using (SqlDataReader dr = cmd.ExecuteReader()) {
+                    while (dr.Read()) { 
+                        
+                        Contacto nuevoContacto = new Contacto();
+
+                        nuevoContacto.IdContacto = Convert.ToInt32(dr["IdContacto"]);
+                        nuevoContacto.Nombres = dr["Nombres"].ToString();
+                        nuevoContacto.Apellidos = dr["Apellidos"].ToString();
+                        nuevoContacto.Telefono = dr["Telefono"].ToString();
+                        nuevoContacto.Correo = dr["Correo"].ToString();
+
+                        olista.Add(nuevoContacto);
+
+                    }
+                }
+            }
+            return View(olista);
+        }
+
+        [HttpGet]
+        public ActionResult Registrar() { 
+        
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int? idcontacto)
+        {
+            if(idcontacto == null)
+                return RedirectToAction("Inicio", "Contacto");
+
+
+            Contacto ocontacto = olista.Where(c => c.IdContacto == idcontacto).FirstOrDefault();
+
+
+            return View(ocontacto);
+        }
+
+        [HttpGet]
+        public ActionResult Eliminar(int? idcontacto)
+        {
+            if (idcontacto == null)
+                return RedirectToAction("Inicio", "Contacto");
+
+
+            Contacto ocontacto = olista.Where(c => c.IdContacto == idcontacto).FirstOrDefault();
+            return View(ocontacto);
+        }
+
+
+
+
+
+
+
+
+        [HttpPost]
+        public ActionResult Registrar(Contacto ocontacto)
+        {
+            using (SqlConnection oconexion = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("sp_Registrar", oconexion);
+                cmd.Parameters.AddWithValue("Nombres", ocontacto.Nombres);
+                cmd.Parameters.AddWithValue("Apellidos", ocontacto.Apellidos);
+                cmd.Parameters.AddWithValue("Telefono", ocontacto.Telefono);
+                cmd.Parameters.AddWithValue("Correo", ocontacto.Correo);
+                cmd.CommandType = CommandType.StoredProcedure;
+                oconexion.Open();
+                cmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Inicio", "Contacto");
+        }
+
+
+        [HttpPost]
+        public ActionResult Editar(Contacto ocontacto)
+        {
+            using (SqlConnection oconexion = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("sp_Editar", oconexion);
+                cmd.Parameters.AddWithValue("IdContacto", ocontacto.IdContacto);
+                cmd.Parameters.AddWithValue("Nombres", ocontacto.Nombres);
+                cmd.Parameters.AddWithValue("Apellidos", ocontacto.Apellidos);
+                cmd.Parameters.AddWithValue("Telefono", ocontacto.Telefono);
+                cmd.Parameters.AddWithValue("Correo", ocontacto.Correo);
+                cmd.CommandType = CommandType.StoredProcedure;
+                oconexion.Open();
+                cmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Inicio", "Contacto");
+        }
+
+
+        [HttpPost]
+        public ActionResult Eliminar(string IdContacto)
+        {
+            using (SqlConnection oconexion = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("sp_Eliminar", oconexion);
+                cmd.Parameters.AddWithValue("IdContacto", IdContacto);
+                cmd.CommandType = CommandType.StoredProcedure;
+                oconexion.Open();
+                cmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Inicio", "Contacto");
+        }
+    }
+}
---
+++ ProyectoCrud/Models/Contacto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoCrud.Models
+{
+    public class Contacto
+    {
+        public int IdContacto { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+
+    }
+}
